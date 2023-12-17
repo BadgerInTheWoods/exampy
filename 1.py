b3d7165ea1d18507c92f8b9b7d6dca5ec5e54bd3
Alexander Badger <sashazateev444@gmail.com>
2023-12-17 12:50:18 +0100

    Done

--- 1.py
+++ 1.py
@@ -5,5 +5,23 @@ lst = ['robot'] * 10
 lst += ['human'] * 10
 random.shuffle(lst)
 data = pd.DataFrame({'whoAmI':lst})
-print(data.head())
+data.head()
 
+pd.get_dummies(data["whoAmI"])
+
+val = list(data['whoAmI'].values)
+
+for i in range(len(val)):
+    if val[i] == "robot":
+        val[i] = 1
+    else: val[i] = 0
+
+def reverse(c):
+    if c == 0:
+        c = 1
+    else:
+        c = 0
+    return c
+
+x = pd.DataFrame({"robot": val, "human": [reverse(i) for i in val]})
+print(x)
